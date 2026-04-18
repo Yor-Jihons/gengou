e0c42bd1f76bf05308baa65637358bb3f78757ea
Yor-Jihons <106572468+Yor-Jihons@users.noreply.github.com>
2026-04-18 17:29:38 +0900

    feat: 日付の追加

--- src/main.ts
+++ src/main.ts
@@ -42,6 +42,17 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <option value="18">18日</option>
     <option value="19">19日</option>
     <option value="20">20日</option>
+    <option value="21">21日</option>
+    <option value="22">22日</option>
+    <option value="23">23日</option>
+    <option value="24">24日</option>
+    <option value="25">25日</option>
+    <option value="26">26日</option>
+    <option value="27">27日</option>
+    <option value="28">28日</option>
+    <option value="29">29日</option>
+    <option value="30">20日</option>
+
   </select>
 </div>
 `
